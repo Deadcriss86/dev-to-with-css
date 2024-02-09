7a99e6d6fc3790b0f486452d6b71c8ccd405bb0b
David Marroquin <nightvariedades@gmail.com>
2024-02-09 10:54:43 -0600

    add get post

--- JS/post.js
+++ JS/post.js
@@ -1,77 +1,32 @@
-document.querySelectorAll("#post-form input").forEach((input) => {
-    input.addEventListener("focus", () => {
-      document.getElementById("form-alert").classList.add("d-none");
-    });
-  });
+// Datos a enviar
+const data = {
+  user: "nombreUsuario",
+  detalles: "detalles del contenido",
+  urlImage: "https://ejemplo.com/imagen.jpg",
+  titulo: "Título del contenido"
+};
 
-  let saveButton = document.getElementById("save-post")
-  let titleField = document.getElementById("title");
-  let descriptionField = document.getElementById("description");
-  let nameField = document.getElementById("name")
-  let imgField = document.getElementById("img")
-  let dateField = document.getElementById("date")
-  let returnButton = document.getElementById("return-post")
+// Configuración del fetch
+const requestOptions = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(data)
+};
 
+// URL a la que enviar los datos
+const url = 'http://localhost:3002/post';
 
-  const createPostObject = () => {
-
-    let postObject = { title, description, name ,img ,imgprofile,date};
-    titleField.value=""
-    descriptionField.value=""
-    nameField.value=""
-    imgField.value=""
-    dateField.value=""
-    if (titleField.value && descriptionField.value && nameField.value && imgField.value && dateField.value) {
-      return postObject;
-    } else {
-      document.getElementById("form-alert").classList.remove("d-none");
-    }
-  };
-
-  const createPostInDb = async (postObject) => {
-   
-    let response = await fetch(
-      "http://localhost:3002/post",
-      {
-        method: "POST",
-        body: JSON.stringify(postObject),
-      }
-    );
-    let data = await response.json();
-    return data;
-  };
-  
-  saveButton.addEventListener("click", async (event) => {
-    event.preventDefault();
-    let title = titleField.value
-    let description = descriptionField.value
-    let name = nameField.value
-    let img = imgField.value
-    let date = dateField.value
-
-    let postObject = {title, description, name, img, date}
-  
-    console.log(postObject);
-    let result = await createPostInDb(postObject);
-    //console.log(result);
-    alert("post publicado!")
-    clearForm();
-  });
-
-  const clearForm=()=>{
-    titleField.value=("")
-    descriptionField.value=("")
-    nameField.value=("")
-    imgField.value=("")
-    dateField.value=("")
-  }
-  let post_public = "https://desafio30js-default-rtdb.firebaseio.com/posts/.json";
-  let usersWrapper = document.getElementById("users-wrapper");
-
-  returnButton.addEventListener("click", ()=>{
-    window.open("/index.html","_self")
+// Realizar la solicitud fetch
+fetch(url, requestOptions)
+  .then(response => response.json())
+  .then(data => {
+    console.log('Respuesta del servidor:', data);
   })
-  
+  .catch(error => {
+    console.error('Error al enviar los datos:', error);
+  });
   
   
   
